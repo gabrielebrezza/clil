51151a4df8d0a882bc69b784b16fa0956ed811c0
gabriele brezza <128699604+gabrielebrezza@users.noreply.github.com>
2025-05-16 00:02:50 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -14,4 +14,46 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('logo').addEventListener('click', () => {
         window.location.assign('/')
     });
+    
+    const audioController = document.getElementById('audioController');
+    const audioControllerIcon = audioController.querySelector('span');
+    const player = document.getElementById('player');
+
+    let isPlaying = false;
+    
+    const wasPlaying = sessionStorage.getItem('isPLaying') === 'true';
+    const storedTime = parseFloat(sessionStorage.getItem('audioTime') || '0');
+
+    if (player) {
+      player.currentTime = storedTime;
+
+      if (wasPlaying) {
+        player.play().then(() => {
+          isPlaying = true;
+          audioController.classList.add('playing');
+          audioControllerIcon.innerText = 'volume_up';
+        }).catch((err) => {
+          console.warn("Autoplay bloccato dal browser", err);
+        });
+      }
+    }
+
+    audioController.addEventListener('click', () => {
+      if (isPlaying) {
+        player.pause();
+        audioControllerIcon.innerText = 'volume_off';
+        audioController.classList.remove('playing');
+      } else {
+        player.play();
+        audioControllerIcon.innerText = 'volume_up';
+        audioController.classList.add('playing');
+      }
+      isPlaying = !isPlaying;
+    });
+
+    window.addEventListener('beforeunload', () => {
+      sessionStorage.setItem('isPLaying', isPlaying.toString());
+      sessionStorage.setItem('audioTime', player.currentTime.toString());
+    });
+
 });
